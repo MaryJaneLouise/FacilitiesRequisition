46de7aeae7438804c12f26b4f5d6eed55f455c4e
MaryJane <mj.louise007@gmail.com>
2023-05-17 11:36:09 +0800

    added files for creating request paper

---
+++ FacilitiesRequisition/Models/FacilityRequests/Expense.cs
@@ -0,0 +1,5 @@
+namespace FacilitiesRequisition.Models.FacilityRequests; 
+
+public class Expense {
+    
+}
---
+++ FacilitiesRequisition/Pages/CreateRequestPaper/Index.cshtml.cs
@@ -0,0 +1,9 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace FacilitiesRequisition.Pages.CreateRequestPaper; 
+
+public class Index : PageModel {
+    public void OnGet() {
+        
+    }
+}
