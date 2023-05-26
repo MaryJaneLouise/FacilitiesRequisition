0df4177e00eb1fbd0e303de78aa8af0ee7a97478
MaryJane <mj.louise007@gmail.com>
2023-05-26 14:24:06 +0800

    [WIP] updating the navbar functionality

--- FacilitiesRequisition/Pages/Dashboard/Index.cshtml.cs
+++ FacilitiesRequisition/Pages/Dashboard/Index.cshtml.cs
@@ -10,62 +10,22 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
 
-
 namespace FacilitiesRequisition.Pages.Dashboard; 
 
-public class IndexModel : PageModel  {
+public class IndexModel : PageModel {
+    public void OnGet() {
+        
+    }
+}
+
+public class NavigationBarModel
+{
     private readonly DatabaseContext _context;
 
-    public IndexModel(DatabaseContext context)  {
+    public NavigationBarModel(DatabaseContext context)
+    {
         _context = context;
     }
 
-    [BindProperty]
-    public string Name { get; set; }
-
-    public string UserType { get; set; }
-    public string IsSuperAdmin { get; set; }
-
-    public IActionResult OnGet()  {
-        var user = HttpContext.Session.GetLoggedInUser(_context);
-        bool isSuperAdministrator = user.Type == Models.UserType.Administrator &&
-            _context.GetAdministratorRoles(user).Any(x => x.Position == AdministratorPosition.SuperAdmin);
-        var userType = isSuperAdministrator ? "Super Administrator" : 
-            user.Type == Models.UserType.Administrator ? "Administrator" :
-            user.Type == Models.UserType.Faculty ? "Faculty" : "Organization Officer";
-        
-        switch (user) {
-            case null:
-                return RedirectToPage("../Login/Index");
-            default:
-                Name = $"{user.FirstName} {user.LastName}";
-                UserType = $"{userType}";
-                return Page();
-        }
-    }
-    
-    public IActionResult OnPostCreateAccount()  {
-        return RedirectToPage("../CreateUsers/Index");
-    }
-
-    public IActionResult OnPostManageUserAccount() {
-        return RedirectToPage("../ManageUsers/Index");
-    }
-
-    public IActionResult OnPostViewProfile() {
-        return RedirectToPage("../ManageUsers/ViewProfile");
-    }
-    
-    public IActionResult OnPostLogout() {
-        HttpContext.Session.Logout();
-        return RedirectToPage("../Login/Index");
-    }
-
-    public IActionResult OnPostRequestPaper() {
-        return RedirectToPage("../CreateRequestPaper/Index");
-    }
-
-    public IActionResult OnPostHome() {
-        return RedirectToPage("../Dashboard/MainScreen");
-    }
+    // Add your properties and methods here that require database access
 }
---
+++ FacilitiesRequisition/Pages/Shared/_NavigationBar.cshtml.cs
@@ -0,0 +1,72 @@
+using System.ComponentModel.DataAnnotations;
+using FacilitiesRequisition.Data;
+using FacilitiesRequisition.Models;
+using FacilitiesRequisition.Models.Administrators;
+using FacilitiesRequisition.Models.Faculties;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace FacilitiesRequisition.Pages.Shared {
+    public class _NavigationBarModel : PageModel  {
+        private readonly DatabaseContext _context;
+    
+        public _NavigationBarModel(DatabaseContext context)  {
+            _context = context;
+        }
+    
+        [BindProperty]
+        public string Name { get; set; }
+    
+        public string UserType { get; set; }
+        public string IsSuperAdmin { get; set; }
+    
+        public IActionResult OnGet()  {
+            var user = HttpContext.Session.GetLoggedInUser(_context);
+            bool isSuperAdministrator = user.Type == Models.UserType.Administrator &&
+                _context.GetAdministratorRoles(user).Any(x => x.Position == AdministratorPosition.SuperAdmin);
+            var userType = isSuperAdministrator ? "Super Administrator" : 
+                user.Type == Models.UserType.Administrator ? "Administrator" :
+                user.Type == Models.UserType.Faculty ? "Faculty" : "Organization Officer";
+            
+            switch (user) {
+                case null:
+                    return RedirectToPage("../Login/Index");
+                default:
+                    Name = $"{user.FirstName} {user.LastName}";
+                    UserType = $"{userType}";
+                    return Page();
+            }
+        }
+        
+        public IActionResult OnPostCreateAccount()  {
+            return RedirectToPage("../CreateUsers/Index");
+        }
+    
+        public IActionResult OnPostManageUserAccount() {
+            return RedirectToPage("../ManageUsers/Index");
+        }
+    
+        public IActionResult OnPostViewProfile() {
+            return RedirectToPage("../ManageUsers/ViewProfile");
+        }
+        
+        public IActionResult OnPostLogout() {
+            HttpContext.Session.Logout();
+            return RedirectToPage("../Login/Index");
+        }
+    
+        public IActionResult OnPostRequestPaper() {
+            return RedirectToPage("../CreateRequestPaper/Index");
+        }
+    
+        public IActionResult OnPostHome() {
+            return RedirectToPage("../Dashboard/MainScreen");
+        }
+    }
+}
+
